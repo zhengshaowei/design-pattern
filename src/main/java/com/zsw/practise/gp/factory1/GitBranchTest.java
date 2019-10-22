5227c1d5636780bcdbddb4cc831b94d34b091c7c
郑少伟 <550989037@qq.com>
2019-10-23 00:03:29 +0800

    提交分支第一个类

---
+++ src/main/java/com/zsw/practise/gp/factory1/GitBranchTest.java
@@ -0,0 +1,12 @@
+package com.zsw.practise.gp.factory1;
+
+/**
+ * @author zheng.shaowei
+ * @create 2019-10-23 0:00
+ **/
+public class GitBranchTest {
+
+    public static void main(String[] args) {
+        System.out.println("学习git分支！");
+    }
+}
